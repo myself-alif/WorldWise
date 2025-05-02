3f8b1f472f0465660cc5202516e708ca36bc290b
Al Mahmud Alif <myself.amalif@gmail.com>
2025-05-03 00:27:45 +0600

    image path fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,12 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/WorldWise">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      {/* <img src="/logo.png" alt="WorldWise logo" className={styles.logo} /> */}
+      <img
+        className={styles.logo}
+        src={`${import.meta.env.BASE_URL}logo.png`}
+        alt="Logo"
+      />
     </Link>
   );
 }
