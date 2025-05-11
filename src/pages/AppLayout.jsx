ab11c2389d8001ee1adc55bb04e6418938bc9ff1
Al Mahmud Alif <myself.amalif@gmail.com>
2025-05-11 17:37:04 +0600

    updated

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,11 +2,21 @@ import styles from "./AppLayout.module.css";
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 import User from "../components/User";
+import { useState } from "react";
+import Button from "../components/Button";
 
 export default function AppLayout() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  function handleClick() {
+    setIsSidebarVisible((val) => !val);
+  }
+
   return (
     <div className={styles.app}>
-      <Sidebar />
+      <Button type="sticky" onClick={handleClick}>
+        {isSidebarVisible ? "Hide" : "Show"}
+      </Button>
+      {isSidebarVisible && <Sidebar />}
       <Map />
       <User />
     </div>
